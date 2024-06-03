5a334d447ac9932016cae5fde058483246c78dae
fredluggard <fredrick.ebuka33@gmail.com>
2024-06-03 19:22:14 +0100

    Made the cart persistant

--- src/features/CartState.jsx
+++ src/features/CartState.jsx
@@ -1,12 +1,22 @@
 /* eslint-disable react-refresh/only-export-components */
 import { createSlice } from "@reduxjs/toolkit";
 
+const startItem =
+  localStorage.getItem("cart") !== null
+    ? JSON.parse(localStorage.getItem("cart"))
+    : [];
+
 export const cartSlice = createSlice({
   name: "cart",
-  initialState: { items: [] },
+  initialState: { items: startItem },
   reducers: {
     setCart: (state, action) => {
       state.items = [...state.items, action.payload];
+
+      localStorage.setItem(
+        "cart",
+        JSON.stringify(state.items.map((item) => item))
+      );
     },
     removeFromCart: (state, action) => {
       state.items = state.items.filter(
